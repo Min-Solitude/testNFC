f7531456408b03617e39f72675b5f36568a1be27
Min solitude <121474260+Min-Solitude@users.noreply.github.com>
2023-11-07 10:12:43 +0700

    dcc

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log(1);
+const btn = document.getElementById("btn_scan");
+
+const ndef = new NDEFReader();
+
+btn.addEventListener("click", async () => {
+  //   check if NFC is supported
+  if ("NDEFReader" in window) {
+    try {
+      await ndef.write("Hello world!");
+      console.log("Message written");
+    } catch (error) {
+      console.log(error);
+    }
+  } else {
+    alert("NFC not supported");
+  }
+});
