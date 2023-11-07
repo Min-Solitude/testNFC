ece3e19cc22ed51cd22d309876dfe011d85592c7
Min solitude <121474260+Min-Solitude@users.noreply.github.com>
2023-11-07 12:59:58 +0700

    hellsadoa

--- index.js
+++ index.js
@@ -45,13 +45,35 @@ btn_scan.addEventListener("click", async () => {
 btn_write.addEventListener("click", async () => {
   alert("User clicked write button");
 
+  const ndef = new NDEFReader();
+  ndef.onreading = (event) => alert("We read a tag!");
+
+  function write(data, { timeout } = {}) {
+    return new Promise((resolve, reject) => {
+      const ctlr = new AbortController();
+      ctlr.signal.onabort = () => reject("Time is up, bailing out!");
+      setTimeout(() => ctlr.abort(), timeout);
+
+      ndef.addEventListener(
+        "reading",
+        (event) => {
+          ndef.write(data, { signal: ctlr.signal }).then(resolve, reject);
+        },
+        { once: true }
+      );
+    });
+  }
+
+  await ndef.scan();
   try {
-    const ndef = new NDEFReader();
-    await ndef.write(
-      "https://translate.google.com/?hl=vi&sl=en&tl=vi&text=An%20error%20has%20occurred%20during%20the%20writing%20process&op=translate"
+    // Let's wait for 5 seconds only.
+    await write(
+      "https://translate.google.com/?hl=vi&sl=en&tl=vi&text=An%20error%20has%20occurred%20during%20the%20writing%20process&op=translate",
+      { timeout: 5_000 }
     );
-    alert("> Message written");
-  } catch (error) {
-    alert("Argh! " + error);
+  } catch (err) {
+    console.error("Something went wrong", err);
+  } finally {
+    console.log("We wrote to a tag!");
   }
 });
