53629368067db28794449d77db92e19c29716de7
Min solitude <121474260+Min-Solitude@users.noreply.github.com>
2023-11-07 10:24:27 +0700

    amdsas

--- index.js
+++ index.js
@@ -1,17 +1,48 @@
-const btn = document.getElementById("btn_scan");
+const btn_scan = document.getElementById("btn_scan");
+const btn_write = document.getElementById("btn_write");
+const btn_read = document.getElementById("btn_read");
 
-const ndef = new NDEFReader();
+btn_scan.addEventListener("click", async () => {
+  alert("User clicked scan button");
 
-btn.addEventListener("click", async () => {
-  //   check if NFC is supported
-  if ("NDEFReader" in window) {
-    try {
-      await ndef.write("Hello world!");
-      console.log("Message written");
-    } catch (error) {
-      console.log(error);
-    }
-  } else {
-    alert("NFC not supported");
+  try {
+    const ndef = new NDEFReader();
+    await ndef.scan();
+    alert("> Scan started");
+
+    ndef.addEventListener("readingerror", () => {
+      alert("Argh! Cannot read data from the NFC tag. Try another one?");
+    });
+
+    ndef.addEventListener("reading", ({ message, serialNumber }) => {
+      alert(`> Serial Number: ${serialNumber}`);
+      alert(`> Records: (${message.records.length})`);
+    });
+  } catch (error) {
+    alert("Argh! " + error);
+  }
+});
+
+btn_write.addEventListener("click", async () => {
+  alert("User clicked write button");
+
+  try {
+    const ndef = new NDEFReader();
+    await ndef.write("Hello world!");
+    alert("> Message written");
+  } catch (error) {
+    alert("Argh! " + error);
+  }
+});
+
+btn_read.addEventListener("click", async () => {
+  alert("User clicked make read-only button");
+
+  try {
+    const ndef = new NDEFReader();
+    await ndef.makeReadOnly();
+    alert("> NFC tag has been made permanently read-only");
+  } catch (error) {
+    alert("Argh! " + error);
   }
 });
