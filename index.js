2d446548cf49251cafe5f60167dbd3017c7425de
Min solitude <121474260+Min-Solitude@users.noreply.github.com>
2023-11-07 10:53:56 +0700

    sadas

--- index.js
+++ index.js
@@ -18,18 +18,13 @@ btn_scan.addEventListener("click", async () => {
 
     ndef.addEventListener("reading", ({ message, serialNumber }) => {
       // show_data.innerHTML = `> Serial Number: ${serialNumber}\n> Records: (${message.records.length})\n`;
-      // for (const record of message.records) {
-      //   show_data.innerHTML += "> Record type:  " + record.recordType + "\n";
-      //   show_data.innerHTML += "> MIME type:    " + record.mediaType + "\n";
-      //   show_data.innerHTML += "> Record id:    " + record.id + "\n";
-      //   show_data.innerHTML += "> Record data:  " + record.data + "\n";
-      // }
-      // I want show url in card NFC
-
-      const decoder = new TextDecoder(record.encoding);
-      alert(decoder.decode(record.data));
-      //   show_data.innerHTML +=
-      //     "> Decoded data: " + decoder.decode(record.data) + "\n";
+      for (const record of message.records) {
+        // show_data.innerHTML += "> Record type:  " + record.recordType + "\n";
+        // show_data.innerHTML += "> MIME type:    " + record.mediaType + "\n";
+        // show_data.innerHTML += "> Record id:    " + record.id + "\n";
+        show_data.innerHTML +=
+          "> Record data:  " + record.data.toString() + "\n";
+      }
     });
   } catch (error) {
     alert("Argh! " + error);
