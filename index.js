d7c7fd91219e9ec3b77371bea9d2fb239976f9ab
Min solitude <121474260+Min-Solitude@users.noreply.github.com>
2023-11-12 11:39:01 +0700

    update 3

--- index.js
+++ index.js
@@ -3,50 +3,15 @@ const id = new URLSearchParams(location.search).get("id");
 console.log(id); // 1
 console.log(typeof id); // 1
 
-const btn_scan = document.getElementById("btn_scan");
 const btn_write = document.getElementById("btn_write");
-const btn_read = document.getElementById("btn_read");
-
-const show_data = document.getElementById("Show_data");
-const show_log = document.getElementById("Show_log");
-
-btn_scan.addEventListener("click", async () => {
-  try {
-    const ndef = new NDEFReader();
-    await ndef.scan();
-    alert("Vui lòng đặt thẻ vào điện thoại");
-
-    ndef.addEventListener("readingerror", () => {
-      alert("Argh! Cannot read data from the NFC tag. Try another one?");
-    });
-
-    ndef.addEventListener("reading", ({ message, serialNumber }) => {
-      alert(serialNumber);
-
-      for (const record of message.records) {
-        if (record.recordType === "text") {
-          const textBytes = new Uint8Array(record.data.buffer);
-          const text = new TextDecoder("utf-8").decode(textBytes);
-          show_data.innerHTML = text;
-          alert(`> Text: ${text}`);
-        } else {
-          const textBytes = new Uint8Array(record.data.buffer);
-
-          const text = new TextDecoder("utf-8").decode(textBytes);
-
-          show_data.innerHTML = text;
-
-          alert(`> Text: ${text}`);
-        }
-      }
-    });
-  } catch (error) {
-    alert("Argh! " + error);
-  }
-});
+const start = document.getElementById("start");
 
 btn_write.addEventListener("click", async () => {
-  alert("Vui lòng đặt thẻ vào điện thoại");
+  start.style = `
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  `;
 
   const ndef = new NDEFReader();
 
@@ -55,6 +20,10 @@ btn_write.addEventListener("click", async () => {
   });
 
   alert("Truyền dữ liệu thành công");
+
+  start.style = `
+    display: none;
+  `;
 
   ndef.addEventListener("readingerror", () => {
     alert("Địt mẹ lỗi! Vui lòng thử lại?");
